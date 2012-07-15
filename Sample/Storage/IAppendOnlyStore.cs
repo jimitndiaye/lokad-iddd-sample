4992d97d7dd37dee15610e905d440c84c8962401
Rinat Abdullin <rinat.abdullin@gmail.com>
2012-07-15 13:14:34 +0200

    Clarifying names over the append-only store

--- Sample/Storage/IAppendOnlyStore.cs
+++ Sample/Storage/IAppendOnlyStore.cs
@@ -6,19 +6,43 @@ namespace Sample
 {
     public interface IAppendOnlyStore : IDisposable
     {
-        void Append(string name, byte[] data, int expectedVersion = -1);
-        IEnumerable<DataWithVersion> ReadRecords(string name, int afterVersion, int maxCount);
-        IEnumerable<DataWithName> ReadRecords(int afterVersion, int maxCount);
+        /// <summary>
+        /// <para>
+        /// Appends data to the stream with the specified name. If <paramref name="expectedStreamVersion"/> is supplied and
+        /// it does not match server version, then <see cref="AppendOnlyStoreConcurrencyException"/> is thrown.
+        /// </para> 
+        /// </summary>
+        /// <param name="streamName">The name of the stream, to which data is appended.</param>
+        /// <param name="data">The data to append.</param>
+        /// <param name="expectedStreamVersion">The server version (supply -1 to append without check).</param>
+        /// <exception cref="AppendOnlyStoreConcurrencyException">thrown when expected server version is
+        /// supplied and does not match to server version</exception>
+        void Append(string streamName, byte[] data, long expectedStreamVersion = -1);
+        /// <summary>
+        /// Reads the records by stream name.
+        /// </summary>
+        /// <param name="streamName">The key.</param>
+        /// <param name="afterVersion">The after version.</param>
+        /// <param name="maxCount">The max count.</param>
+        /// <returns></returns>
+        IEnumerable<DataWithVersion> ReadRecords(string streamName, long afterVersion, int maxCount);
+        /// <summary>
+        /// Reads the records across all streams.
+        /// </summary>
+        /// <param name="afterVersion">The after version.</param>
+        /// <param name="maxCount">The max count.</param>
+        /// <returns></returns>
+        IEnumerable<DataWithName> ReadRecords(long afterVersion, int maxCount);
 
         void Close();
     }
 
     public sealed class DataWithVersion
     {
-        public readonly int Version;
+        public readonly long Version;
         public readonly byte[] Data;
 
-        public DataWithVersion(int version, byte[] data)
+        public DataWithVersion(long version, byte[] data)
         {
             Version = version;
             Data = data;
@@ -37,15 +61,15 @@ namespace Sample
     }
 
     /// <summary>
-    /// Is thrown internally, when storage version does not match the condition specified in <see cref="TapeAppendCondition"/>
+    /// Is thrown internally, when storage version does not match the condition 
+    /// specified in server request
     /// </summary>
     [Serializable]
     public class AppendOnlyStoreConcurrencyException : Exception
     {
-
-        public int ExpectedVersion { get; private set; }
-        public int ActualVersion { get; private set; }
-        public string Name { get; private set; }
+        public int ExpectedStreamVersion { get; private set; }
+        public int ActualStreamVersion { get; private set; }
+        public string StreamName { get; private set; }
 
         protected AppendOnlyStoreConcurrencyException(
             SerializationInfo info,
@@ -56,9 +80,9 @@ namespace Sample
             : base(
                 string.Format("Expected version {0} in stream '{1}' but got {2}", expectedVersion, name, actualVersion))
         {
-            Name = name;
-            ExpectedVersion = expectedVersion;
-            ActualVersion = actualVersion;
+            StreamName = name;
+            ExpectedStreamVersion = expectedVersion;
+            ActualStreamVersion = actualVersion;
         }
     }
 }
